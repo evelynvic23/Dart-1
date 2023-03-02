c6b3476bdac3af6d3d25bff29dcd0200c520a0c0
Evelyn Victoria da Silva Santos <115710253+evelynvic23@users.noreply.github.com>
2023-03-02 10:57:21 -0300

    Add files via upload

---
+++ leitura.dart
@@ -0,0 +1,67 @@
+
+import 'dart:io';
+import 'dart:math';
+import '';
+
+
+main(){
+
+//   print("Digite seu lindo nome: ");
+//   var nome = stdin.readLineSync();
+// print("Olá: $nome");
+
+
+/////////// Primeiro Execício ///////////////////
+
+
+// print("Digite o valor do Raio: ");
+
+// var raio = stdin.readLineSync();
+
+
+// var raioConvert = double.parse(raio!);
+// var potencia = pow(raioConvert, 2);
+
+// print(4*pi.toDouble()*potencia);
+
+
+
+///////////////////  Segundo Exercício //////////////////////////
+
+
+
+// print("Digite o valor do raio: ");
+
+// var raio = stdin.readLineSync();
+
+// var raioConvertido = double.parse(raio!);
+
+// var pot = pow(raioConvertido, 3);
+
+// var fracao = 4/3;
+
+// print("Valor do Volume: ");
+// print(fracao*pi.toDouble()*pot);
+
+
+
+
+
+//////////////////// Terceiro Exercícios ////////////////////////////
+
+
+// print("Digite o valor do raio: ");
+
+// var raio = stdin.readLineSync();
+
+// var raioConvertido = double.parse(raio!);
+
+// var potencia = pow(raioConvertido, 2);
+
+
+// print("ÁREA: ");
+
+// print(pi.toDouble()*potencia);
+
+
+// } 
