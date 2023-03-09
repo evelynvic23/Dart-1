bcdfb6766a380cfdddde28d6aef48991c33a957d
Evelyn Victoria da Silva Santos <115710253+evelynvic23@users.noreply.github.com>
2023-03-09 11:22:08 -0300

    Update leitura.dart

--- leitura.dart
+++ leitura.dart
@@ -47,7 +47,7 @@ main(){
 
 
 
-//////////////////// Terceiro Exercícios ////////////////////////////
+//////////////////// Terceiro Exercício ////////////////////////////
 
 
 // print("Digite o valor do raio: ");
@@ -62,6 +62,91 @@ main(){
 // print("ÁREA: ");
 
 // print(pi.toDouble()*potencia);
+  
+  
+  //////////////////// Quarto Exercício ////////////////////////////
+
+// var ano = 365;
+// var mes = 30;
+
+// print("Digite sua idade em anos: ");
+
+// var idade = stdin.readLineSync();
+// var idade2 = int.parse(idade!);
+
+// print("Digite seus meses: ");
+
+// var meses = stdin.readLineSync();
+// var meses2 = int.parse(meses!);
+
+// print("Digite seus dias: ");
+
+// var dias = stdin.readLineSync();
+
+// var dias2 = int.parse(dias!);
+
+// var idadeEmDias = idade2*ano + meses2*mes + dias2;
+
+// print("$idadeEmDias dias de vida!");
+
+
+
+
+//////////////////// Quinto Exercício ////////////////////////////
+
+
+// print("Digite a primeira nota :");
+
+//  var nota1 = stdin.readLineSync();
+
+//  var nota1C = int.parse(nota1!);
+
+//  print("Digite a segunda nota :");
+
+//  var nota2 = stdin.readLineSync();
+ 
+//  var nota2C = int.parse(nota2!);
+
+//  print("Digite a terceira nota :");
+
+//  var nota3 = stdin.readLineSync();
+ 
+//  var nota3C = int.parse(nota3!);
+
+
+//  print("Digite a quarta nota :");
+
+//  var nota4 = stdin.readLineSync();
+ 
+//  var nota4C = int.parse(nota4!);
+
+
+//  var media = nota1C + nota2C + nota3C + nota4C /4;
+
+
+//  print("Média aritmetica: $media");
+
+
+
+//////////////////// 7 Exercício ////////////////////////////
+
+// print("Digite a distância percorrida: ");
+// var distancia = stdin.readLineSync();
+
+// var distancia2 = double.parse(distancia!);
+
+// print("Digite o tempo: ");
+// var tempo = stdin.readLineSync();
+
+// var tempo2 = double.parse(tempo!);
+
+
+// var media = distancia2 / tempo2;
+
+// print("A velocidade média é: $media km/h");
+
+  
+  
 
 
 // } 
