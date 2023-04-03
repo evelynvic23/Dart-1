75d0b003e0561f86090d38a649522d57ca51f487
Evelyn Victoria da Silva Santos <115710253+evelynvic23@users.noreply.github.com>
2023-04-03 11:13:34 -0300

    Add files via upload

---
+++ AtividadeDart/AtividadeDart/Atividade.dart
@@ -0,0 +1,184 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:math';
+import '';
+
+
+//Atividade 1
+
+//  void expressoes(){
+//   print("Nome da autora: Evelyn Victoria da Silva Santos");
+// print("Data de nascimento: 23/10/2005");
+
+// print("Digite um número: ");
+// var num1 = stdin.readLineSync();
+
+// var num1Convertido = int.parse(num1!);
+
+// print("Digite outro número: ");
+// var num2 = stdin.readLineSync();
+
+// var num2Convertido = int.parse(num2!);
+
+// print("Escolha uma das opções: |+| |-| |*| |/| |^| |r|");
+// var valores = stdin.readLineSync();
+
+// switch (valores) {
+//   case "+":
+//   var soma = num1Convertido + num2Convertido;
+//    print("Com os valores iremos realizar uma ADIÇÃO: $soma");
+//     break;
+//   case "-":
+//   var sub = num1Convertido - num2Convertido;
+//   print("Com os valores iremos realizar uma SUBTRAÇÃO: $sub");
+//     break;
+//   case "*":
+//   var mult = num1Convertido * num2Convertido;
+//     print("Com os valores iremos realizar uma MULTIPLICAÇÃO: $mult");
+//     break;
+//   case "/":
+//   var div = num1Convertido / num2Convertido;
+//     print("Com os valores iremos realizar uma DIVISÃO: $div");
+//     break;
+//   case "^":
+//   var exp = num1Convertido ^ num2Convertido;
+//     print("Com os valores iremos realizar uma EXPONENCIAÇÃO: $exp");
+//     break;
+//   case "r":
+//   var rad = sqrt(num1Convertido); 
+//   var rad2 = sqrt(num2Convertido);
+//     print("Com os valores iremos realizar uma RADICIAÇÃO: $rad");
+//     print("Com os valores iremos realizar uma RADICIAÇÃO: $rad2");
+    
+//     break;
+  
+//   default:
+// }
+
+
+// }
+
+// void main(){
+//   expressoes();
+// }
+
+
+
+// Atividade 2
+
+// void tabuada(){
+//   var resultado;
+
+// print("Nome da autora: Evelyn Victoria da Silva Santos");
+// print("Data de nascimento: 23/10/2005");
+
+// print("Digite quantas tabuadas deseja: ");
+// var quantT = stdin.readLineSync();
+
+// var quantidade = int.parse(quantT!);
+
+// for(int x = 1; x <= quantidade; x++){
+
+// print("Digite a tabuada desejada: ");
+// var textTabuada = stdin.readLineSync();
+
+// print("Digite o limite da tabuada: ");
+// var textLimite = stdin.readLineSync();
+
+// var tabuada = int.parse(textTabuada!);
+// var limite = int.parse(textLimite!);
+
+
+//   print("Número de vezes escolhido: $x/$quantidade");
+
+// for(int i = 1; i <= limite; i++){
+//   resultado = tabuada * i;
+//   print("$tabuada X $i = $resultado");
+
+// }
+// }
+
+// }
+
+// void main(){
+//   tabuada();
+// }
+
+
+
+// Atividade 3
+
+
+
+// int calcularFatorial() {
+// print("Nome da autora: Evelyn Victoria da Silva Santos");
+// print("Data de nascimento: 23/10/2005");
+// print("Digite um número: ");
+//  var num1 = stdin.readLineSync();
+
+//  var num = int.parse(num1!);
+
+//   int fatorial = 1;
+//   for (int i = 1; i <= num; i++) {
+//     fatorial *= i;
+//   }
+
+//   return fatorial;
+
+// }
+
+// void main(){
+
+// int resultado = calcularFatorial();
+// print("Resultado: $resultado");
+//   calcularFatorial();
+// }
+
+ 
+// Atividade 4
+
+
+// int potencia() {
+//     print("Nome da autora: Evelyn Victoria da Silva Santos");
+// print("Data de nascimento: 23/10/2005");
+//  print("Digite um número: ");
+//   var num1 = stdin.readLineSync();
+
+//  var a = int.parse(num1!);
+
+//  print("Digite outro número: ");
+//   var num2 = stdin.readLineSync();
+
+//  var b = int.parse(num2!);
+
+//   int resultado = 1;
+//   for (int i = 0; i < b; i++) {
+//     resultado *= a;
+//   }
+//   return resultado;
+// }
+// void main() {
+ 
+//   int resultado = potencia();
+//   print("O resultado é: $resultado");
+// }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
